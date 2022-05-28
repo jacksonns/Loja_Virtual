e411846c276dd5fe1c9a615ddd34c55d4bc119dc
Jefferson Lopes <jefferson.lopes@ebanx.com>
2022-05-28 19:33:59 -0300

    added id to user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,14 +1,16 @@
 from src.util.price_calculator import PriceCalculator
 from src.exceptions.budget_exception import NegativeBudgetException
 from src.exceptions.password_exception import InvalidPasswordException
+import time
 
 class User():
-
+    id: int
     username: str
     password: str
     budget: tuple[int, int]
 
     def __init__(self, username: str, password: str, budget: tuple[int, int] = (0,0)):
+        self.id = int(time.time())
         self.username = username
         self.budget = budget
         if (self.validatePassword(password)):
