5b6c307954b802098e7b1824490264dd84759bfd
Jefferson Lopes <jefferson.lopes@ebanx.com>
2022-05-28 20:16:32 -0300

    added log transaction history after the payment execution

--- src/models/purchase.py
+++ src/models/purchase.py
@@ -1,6 +1,8 @@
 from src.models.session import Session
 from src.models.coupon import Coupon
 from src.models.address import Address
+from src.models.transaction_history import TransactionHistory
+from src.util.price_calculator import PriceCalculator
 from src.exceptions.budget_exception import InsufficientBudgetException
 
 class Purchase(): # Representa uma operação de compra feita na loja
@@ -29,7 +31,7 @@ class Purchase(): # Representa uma operação de compra feita na loja
 
     def make_payment(self, total_price):
         self.session.subtract_user_budget(total_price)
-    
+
     def update_sellers_budget(self):
         for item_id in self.session.get_cart_items():
             item = self.session.cart.get_item_by_id(item_id)
@@ -37,9 +39,37 @@ class Purchase(): # Representa uma operação de compra feita na loja
             payed_value = self.session.cart.get_total_price_of_item(item)
             seller.add_budget(payed_value)
 
+    def log_transaction_history(self):
+        sellers_with_costs = dict()
+        buyer_id = self.session.user.id
+        cart_id = self.session.cart.id
+
+        for item_id in self.session.get_cart_items():
+            item = self.session.cart.get_item_by_id(item_id)
+            payed_value = self.session.cart.get_total_price_of_item(item)
+            seller_id = item.get_seller().id
+
+            if seller_id not in sellers_with_costs:
+                sellers_with_costs[seller_id] = dict()
+                sellers_with_costs[seller_id]["shipping_cost"] = self.get_shipping_rate()
+                sellers_with_costs[seller_id]["items_cost"] = (0, 0)
+                sellers_with_costs[seller_id]["total_cost"] = (0, 0)
+
+            sellers_with_costs[seller_id]["items_cost"] = PriceCalculator.sum(sellers_with_costs[seller_id]["items_cost"], payed_value)
+            sellers_with_costs[seller_id]["total_cost"] = PriceCalculator.sum(sellers_with_costs[seller_id]["total_cost"], payed_value)
+
+        history = []
+        for seller_id, seller_costs in sellers_with_costs.items():
+            transaction_history = TransactionHistory(buyer_id)
+            transaction_history.set_transaction_details(seller_id, cart_id, seller_costs["shipping_cost"], seller_costs["items_cost"], seller_costs["total_cost"])
+            history.append(transaction_history)
+
+        return history
+
     def make_purchase(self):
         if self.session.get_user_budget() < self.get_total_price():
             raise InsufficientBudgetException
         else:
             self.make_payment(self.get_total_price())
             self.update_sellers_budget()
+            return self.log_transaction_history()
