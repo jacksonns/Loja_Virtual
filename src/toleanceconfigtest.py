ad8a3777e5704f46829ac3e62ce1fe6303fb12c4
aikaterhine <catarina.edp@gmail.com>
2022-06-26 12:17:38 -0300

    testando configuracao de tolerancia

---
+++ src/toleanceconfigtest.py
@@ -0,0 +1,29 @@
+class NegativeBudgetException(Exception):
+    def __init__(self):
+        message = "Budget cannot be less than 0."
+        super().__init__(message)
+
+class InsufficientBudgetException(Exception):
+    def __init__(self):
+        message = "Insufficient budget to make a purchase."
+        super().__init__(message)
+
+class NegativeBudgetException(Exception):
+    def __init__(self):
+        message = "Budget cannot be less than 0."
+        super().__init__(message)
+
+class InsufficientBudgetException(Exception):
+    def __init__(self):
+        message = "Insufficient budget to make a purchase."
+        super().__init__(message)
+
+class NegativeBudgetException(Exception):
+    def __init__(self):
+        message = "Budget cannot be less than 0."
+        super().__init__(message)
+
+class InsufficientBudgetException(Exception):
+    def __init__(self):
+        message = "Insufficient budget to make a purchase."
+        super().__init__(message)
